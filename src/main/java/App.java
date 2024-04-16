08841b4755b1f34e2e4ebfacd9073a90b624bf6e
Lambari <78892876+AndreiBoeck@users.noreply.github.com>
2024-04-16 20:07:07 -0300

    funfa?

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,99 +6,94 @@ import java.util.Stack;
 public class App {
     public static void main(String[] args) {
         Scanner in = null;
-        try {
-            in = new Scanner(new FileReader(args[0]));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        int largura;
-        int comprimento;
-        largura = Integer.parseInt(in.next());
-        comprimento = Integer.parseInt(in.next());
-        System.out.printf("Largura : %d    | Comprimento : %d\n", largura, comprimento);
-        char[][] caminho = new char[largura][comprimento];
-        int count = 0;
-        while(in.hasNextLine()){
-            String line = in.nextLine();
-            if(!line.isBlank()){
-                caminho[count] = line.replaceAll(" ", "x").toCharArray();
+        String[] arquivos = {"casoJ50.txt","casoJ100.txt","casoJ200.txt","casoJ500.txt","casoJ750.txt","casoJ1000.txt","casoJ1500.txt","casoJ2000.txt",};
+        for (String file :
+                arquivos) {
+            try {
+                in = new Scanner(new FileReader(file));
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
             }
-            count++;
-        }
-
-        int atualL = foundInicio(caminho);
-        int atualC = 0;
-        int direcao = 1;
-        Stack<Character> pilha = new Stack<>();
-
-        while(true){
-            if (caminho[atualL][atualC] == '#') {
-                break;
+            int largura;
+            int comprimento;
+            largura = Integer.parseInt(in.next());
+            comprimento = Integer.parseInt(in.next());
+            System.out.printf("Largura : %d    | Comprimento : %d\n", largura, comprimento);
+            char[][] caminho = new char[largura + 1][comprimento + 1];
+            int count = 0;
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                if (!line.isBlank()) {
+                    caminho[count] = line.replaceAll(" ", "x").toCharArray();
+                }
+                count++;
             }
 
-            pilha.push(caminho[atualL][atualC]);
+            int atualL = foundInicio(caminho);
+            int atualC = 0;
+            int direcao = 1;
+            Stack<Character> pilha = new Stack<>();
 
-            switch (direcao){
-                case 1 -> {
-                    if(caminho[atualL][atualC] == '\\'){
-                        direcao = 2;
-                        atualL += 1;
-                    } else if (caminho[atualL][atualC] == '/') {
-                        direcao = -2;
-                        atualL -= 1;
-                    } else {
-                        atualC += direcao;
-                    }
-                    System.out.println(atualL + "/" + atualC);
-                    System.out.println(caminho[atualL][atualC]);
+            while (true) {
+                if (caminho[atualL][atualC] == '#') {
+                    break;
                 }
-                case -1 -> {
-                    if(caminho[atualL][atualC] == '\\'){
-                        direcao = -2;
-                        atualL -= 1;
-                    } else if (caminho[atualL][atualC] == '/') {
-                        direcao = 2;
-                        atualL += 1;
-                    } else {
-                        atualC += direcao;
+
+                pilha.push(caminho[atualL][atualC]);
+
+                switch (direcao) {
+                    case 1 -> {
+                        if (caminho[atualL][atualC] == '\\') {
+                            direcao = 2;
+                            atualL += 1;
+                        } else if (caminho[atualL][atualC] == '/') {
+                            direcao = -2;
+                            atualL -= 1;
+                        } else {
+                            atualC += direcao;
+                        }
                     }
-                    System.out.println(atualL + "/" + atualC);
-                    System.out.println(caminho[atualL][atualC]);
+                    case -1 -> {
+                        if (caminho[atualL][atualC] == '\\') {
+                            direcao = -2;
+                            atualL -= 1;
+                        } else if (caminho[atualL][atualC] == '/') {
+                            direcao = 2;
+                            atualL += 1;
+                        } else {
+                            atualC += direcao;
+                        }
 
-                }
-                case 2 -> {
-                    if(caminho[atualL][atualC] == '\\'){
-                        direcao = 1;
-                        atualC += 1;
-                    } else if (caminho[atualL][atualC] == '/') {
-                        direcao = -1;
-                        atualC -= 1;
-                    } else {
-                        atualL += direcao-1;
                     }
-                    System.out.println(atualL + "/" + atualC);
-                    System.out.println(caminho[atualL][atualC]);
+                    case 2 -> {
+                        if (caminho[atualL][atualC] == '\\') {
+                            direcao = 1;
+                            atualC += 1;
+                        } else if (caminho[atualL][atualC] == '/') {
+                            direcao = -1;
+                            atualC -= 1;
+                        } else {
+                            atualL += direcao - 1;
+                        }
 
 
-                }
-                case -2 -> {
-                    if(caminho[atualL][atualC] == '\\'){
-                        direcao = -1;
-                        atualC -= 1;
-                    } else if (caminho[atualL][atualC] == '/') {
-                        direcao = 1;
-                        atualC += 1;
-                    } else {
-                        atualL += direcao+1;
                     }
-                    System.out.println(atualL + "/" + atualC);
-                    System.out.println(caminho[atualL][atualC]);
+                    case -2 -> {
+                        if (caminho[atualL][atualC] == '\\') {
+                            direcao = -1;
+                            atualC -= 1;
+                        } else if (caminho[atualL][atualC] == '/') {
+                            direcao = 1;
+                            atualC += 1;
+                        } else {
+                            atualL += direcao + 1;
+                        }
 
+                    }
                 }
             }
-            System.out.println(direcao);
+            System.out.println(soma(pilha));
         }
-        System.out.println(soma(pilha));
     }
 
     private static int foundInicio(char[][] caminho) {
@@ -113,7 +108,7 @@ public class App {
         StringBuilder valor = new StringBuilder();
         int total = 0;
         for (char a:
-             pilha) {
+                pilha) {
             if(a == '-' || a == '/' || a == '\\' || a == '#' || a == '|'){
                 if(!valor.isEmpty()) {
                     total += Integer.parseInt(valor.toString());
